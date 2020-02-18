9edc017bdf8fd0c9cb97d74fec729a78788e47d1
Gihyeon Sung <gi@hyeon.io>
2020-02-19 00:13:51 +0900

    Setup tokyo-metro color scheme

--- config.def.h
+++ config.def.h
@@ -85,30 +85,30 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#1e2132",
+	"#e24240",
+	"#bca375",
+	"#f19a36",
+	"#56b88a",
+	"#8b76d0",
+	"#4399bb",
+	"#abadb3",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#6b7089",
+	"#ea5131",
+	"#c8af81",
+	"#f7a649",
+	"#62c496",
+	"#9885da",
+	"#4fa5c7",
+	"#b5b8c2",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	"#1c1c1c",
+	"#d0d0d0",
 };
 
 
@@ -116,10 +116,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
