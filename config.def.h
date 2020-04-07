b49fbf1d23f6ae3011d853f9d4af30a43ae965f2
Gihyeon Sung <gi@hyeon.io>
2020-04-07 13:35:49 +0900

    Change font to neodgm

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka:pixelsize=16:antialias=true:autohint=true";
+static char *font = "NeoDunggeunmo:pixelsize=16";
 static int borderpx = 32;
 
 /*
