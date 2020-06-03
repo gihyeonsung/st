fa7b126be4d99ffadd09e527f7c01cc7fe2d1da8
Gihyeon Sung <gi@hyeon.io>
2020-06-03 09:26:23 +0900

    Change borderpx to 8 for readability

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static int borderpx = 0;
 static char *font = "JetBrainsMono-13";
+static int borderpx = 8;
 
 /*
  * What program is execed by st depends of these precedence rules:
